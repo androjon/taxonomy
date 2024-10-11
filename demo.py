fa0a3ecdbd647a8dce1e37a10053f96d36c54f37
androjon <jonfindahl@gmail.com>
2024-10-11 08:40:00 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -502,6 +502,7 @@ def post_selected_occupation(id_occupation):
         if selected_region:
             regional_id = st.session_state.regions.get(selected_region)
             relevant_forecast = forecasts.get(regional_id)
+            st.write(regional_id)
         else:
             regional_id = None
             relevant_forecast = forecasts.get("i46j_HmG_v64")
